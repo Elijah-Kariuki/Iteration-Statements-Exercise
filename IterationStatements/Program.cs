ed34bc66b7b881f1c53ac90c9e400dff0d994447
Elijah Kariuki <ElijahKariuki1981@outlook.com>
2022-10-10 01:55:37 -0400

    initial commit

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace IterationStatements
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IterationStatements
 {
     internal class Program
     {
+        public static void int CountTo1000()
+        {
+            var theCount = new List<int>();
+            var currentCount = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                currentCount++;
+                theCount.Add(currentCount);
+                Console.WriteLine( $"{theCount[i]}");
+            }
+            
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(CountTo1000());
         }
     }
 }
